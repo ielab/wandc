198bc3712f524d0c49c7d7b0caf58c5b4c249dfc
ArvinZhuang <shengyaozhuang@gmail.com>
2023-06-27 16:22:34 +1000

    update setup

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        "wandb>=0.15.4",
-        "codecarbon>=2.2.4",
+        "wandb",
+        "codecarbon",
     ]
 )
